74317406f19d0042f2a0df43bbc1492e2fda83eb
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-02-11 13:51:49 +0000

    コントローラーの記述追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
 
+  belongs_to :user
+  belongs_to :environment
+  belongs_to :prefecture
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  
   # activestorageで複数の画像を投稿・保存する記述
   has_many_attached :post_images
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  
   # activestorageでプロフィール画像を保存する記述
   has_one_attached :profile_image
 end
