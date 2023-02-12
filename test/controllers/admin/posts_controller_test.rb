0aa706d09deefe004f5345efb7dfd1b1ea61837e
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-02-12 04:37:29 +0000

    ルーティングの修正

--- test/controllers/admin/posts_controller_test.rb
+++ test/controllers/admin/posts_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Public::FavoritesControllerTest < ActionDispatch::IntegrationTest
+class Admin::PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
