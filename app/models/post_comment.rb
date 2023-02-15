76808cb4b4dfb2f4d400f11b96daa4f77ba9c6a1
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-02-15 14:40:37 +0000

    お気に入りとコメント機能実装

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -4,5 +4,10 @@ class PostComment < ApplicationRecord
   belongs_to :post
   
   # activestorageで複数の画像を投稿・保存する記述
-  has_many_attached :post_comment_images
+  has_many_attached :images
+  
+  def get_images
+    (images.attached?) ? images : 'no-image.png'
+  end
+  
 end
