43cd27a193173acd9ef637ac415980bdff82c548
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-02-21 06:03:10 +0000

    投稿一覧画面の部分テンプレート化・topページのレイアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  #ヘッダーの検索機能
+  def search
+    @environments = Environment.all
+    @prefectures = Prefecture.all
+    @posts = Post.all
+  end
   
   protected
   
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,10 @@
 class Public::HomesController < ApplicationController
+  
   def top
-    @environments = Environment.all
-    @prefectures = Prefecture.all
     @posts = Post.all
   end
 
   def how_to
   end
+  
 end
