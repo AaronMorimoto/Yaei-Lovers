0b923de1bc364b657dabb47d47a2756add864e34
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-03-27 16:22:28 +0000

    管理者側コメントの削除機能修正

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -3,8 +3,14 @@ class Admin::PostCommentsController < ApplicationController
   before_action :set_post
   
   def destroy
-    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to admin_post_path(@post), notice: "コメントの削除に成功しました。"
+    if admin_signed_in
+      PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+      redirect_to admin_post_path(@post), notice: "コメントの削除に成功しました。"
+    else
+      redirect_to root_path, notice: "コメントの削除は管理者のみが行えます。"
+    end
+    # PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    # redirect_to admin_post_path(@post), notice: "コメントの削除に成功しました。"
   end
   
   private
