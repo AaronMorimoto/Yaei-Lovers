f54c80c3db3398e3d4b41750f36994d660c7e0d2
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-04-22 15:26:18 +0000

    コメント画像投稿修正

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,10 +3,8 @@ class Public::PostCommentsController < ApplicationController
   # before_action :set_post
   
   def create
-    # post = Post.find(params[:post_id])
     @post = Post.find(params[:post_id])
     @comment = current_user.post_comments.new(post_comment_params)
-    # @comment.post_id = post.id
     @comment.post_id = @post.id
     if @comment.save
       redirect_to post_path(@post), notice: "コメントの投稿に成功しました。"
@@ -25,32 +23,12 @@ class Public::PostCommentsController < ApplicationController
     end
   end
   
-  # 画像アップロード用のアクション
-  def comment_upload_image
-    @comment_image_blob = create_blob(params[:image])
-    render json: @comment_image_blob
-  end
-  
   
 
   private
   
   def post_comment_params
-    params.require(:post_comment).permit(:comment, :rate, images: []).merge(images: comment_uploaded_images)
-  end
-  
-  # コメントのアップロード済み画像の検索
-  def comment_uploaded_images
-    params[:post_comment][:images].drop(0).map{|id| ActiveStorage::Blob.find(id)} if params[:post_comment][:images]
-  end
-  
-  # blobデータの作成
-  def create_blob(file)
-    ActiveStorage::Blob.create_and_upload!(
-      io: file.open,
-      filename: file.original_filename,
-      content_type: file.content_type
-    )
+    params.require(:post_comment).permit(:comment, :rate, images: [])
   end
   
   # def set_post
